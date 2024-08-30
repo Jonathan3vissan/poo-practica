19b06c2e070ff64162c5e034b4a1cbbc3238f664
Jonnthan3visan <joptrabajo@hotmail.com>
2024-08-30 19:52:27 -0300

    sistema de inventario
    incianco desarrollo

---
+++ SistemaInventario/SistemaInDeInventario.js
@@ -0,0 +1,29 @@
+class SitemaInventario {
+    #productos = [];
+
+
+    agregarProductos(nuevoNombre, agregarCantindad) {
+        for (let i = 0; i < this.#productos.length; i++) {
+            if (!this.#productos[i].nombre.includes(nuevoNombre)) {
+                this.#productos[i].nombre = nuevoNombre;
+                this.#productos[i].cantidad = agregarCantindad;
+            } else {
+                this.#productos[i].cantidad += agregarCantindad;
+            }
+        }
+    }
+
+
+
+    venderProducto(nombreVEnta, cantidadVenta) {
+
+
+
+    }
+
+    consultaInventario() {
+        for (let i = 0; i < this.#productos.length; i++) {
+            console.log(`nombre porducto:${this.#productos[i].nombre} cantidad en stock: ${this.#productos[i], cantidad}`);
+        }
+    }
+}
