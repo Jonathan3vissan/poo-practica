d36fa6e9bdc08af92f2cd6221c62960165639eb8
Jonnthan3visan <joptrabajo@hotmail.com>
2024-08-29 23:08:50 -0300

    creo circulo
    calculo circunferencia

--- practica.js
+++ practica.js
@@ -2,10 +2,38 @@ class Persona {
     nombre = "ernesto";
     edad = 23;
     Saludar() {
-        return "hola soy " + this.nombre + "y tengo " + this.edad + "años"
+        return "hola soy " + this.nombre + " " + "y tengo " + this.edad + "" + " años"
     }
 }
 const persona = new Persona();
 
 
 console.log(persona.Saludar());
+
+
+
+class Circulo {
+    radio = 0;
+
+
+    calcularCircunferencia() {
+        let circunferencia = 2 * Math.PI * this.radio
+        return circunferencia
+    }
+
+
+    setRadio(valor) {
+        this.radio = valor
+    }
+
+
+}
+
+
+const circulo = new Circulo();
+let numero = 10;
+
+circulo.setRadio(numero);
+
+console.log(circulo.calcularCircunferencia());
+
