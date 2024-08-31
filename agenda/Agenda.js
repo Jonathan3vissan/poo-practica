a7deb960dc81e8b08b00583fe560da7a9d217364
Jonnthan3visan <joptrabajo@hotmail.com>
2024-08-31 10:00:16 -0300

    agenda
    metodos agregar contacto y ver lista de contacto finalizadas

---
+++ agenda/Agenda.js
@@ -0,0 +1,52 @@
+class Agenda {
+    #contacto = [
+        {
+            nombre: "def nombre",
+            telefono: 0,
+        }
+    ]
+
+    agregarContacto(nombre, telefono) {
+        let nuevoContacto = {
+            nombre: nombre,
+            telefono: telefono
+        };
+        let contactoEncontrado = false;
+        let p = 0;
+        for (let i = 0; i < this.#contacto.length; i++) {
+            if (this.#contacto[i].nombre === nombre) {
+                contactoEncontrado = true;
+                p = i;
+            }
+        }
+        if (contactoEncontrado) {
+            this.#contacto[p].telefono = telefono
+            console.log("contacto existente,actuaizaciond e numeor de telefono exitoso");
+
+        } else {
+            if (this.#contacto[0].nombre === "def nombre") {
+                this.#contacto.shift();
+                console.log("elimino el def de fabrica");
+                
+            }
+            this.#contacto.push(nuevoContacto)
+            console.log("contacto agredo exitosamente");
+        }
+    }
+
+
+
+    listaDeContactos() {
+        return this.#contacto
+    }
+
+
+}
+const agendacontactos = new Agenda();
+let nombre = "estaban ";
+let numeroTelefno = 1154891235;
+
+agendacontactos.agregarContacto(nombre, numeroTelefno)
+
+
+console.table(agendacontactos.listaDeContactos())
